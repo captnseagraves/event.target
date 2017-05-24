b1b3994d134f17e956268bc7a4bbc191d6f898f8
TomGarrison <Tomgarrison2011@gmail.com>
2017-05-24 16:59:11 -0600

    cards

--- public/bundle.js
+++ public/bundle.js
@@ -26104,10 +26104,10 @@
 /* 231 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -26125,32 +26125,77 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var EventCard = function (_React$Component) {
-	  _inherits(EventCard, _React$Component);
+	    _inherits(EventCard, _React$Component);
 
-	  function EventCard() {
-	    _classCallCheck(this, EventCard);
+	    function EventCard() {
+	        _classCallCheck(this, EventCard);
 
-	    return _possibleConstructorReturn(this, (EventCard.__proto__ || Object.getPrototypeOf(EventCard)).apply(this, arguments));
-	  }
-
-	  _createClass(EventCard, [{
-	    key: 'render',
-	    value: function render() {
-
-	      return _react2.default.createElement(
-	        'div',
-	        null,
-	        _react2.default.createElement(
-	          'h1',
-	          null,
-	          this.props.event.name
-	        ),
-	        _react2.default.createElement('hr', null)
-	      );
+	        return _possibleConstructorReturn(this, (EventCard.__proto__ || Object.getPrototypeOf(EventCard)).apply(this, arguments));
 	    }
-	  }]);
 
-	  return EventCard;
+	    _createClass(EventCard, [{
+	        key: "render",
+	        value: function render() {
+	            console.log(this.props.event);
+	            return _react2.default.createElement(
+	                "div",
+	                { className: "col-sm-6 col-md-4 col-lg-3 mt-4" },
+	                _react2.default.createElement(
+	                    "div",
+	                    { className: "card" },
+	                    _react2.default.createElement("img", { className: "card-img-top", src: this.props.event.cover_picture }),
+	                    _react2.default.createElement("br", null),
+	                    _react2.default.createElement(
+	                        "div",
+	                        { className: "card-block" },
+	                        _react2.default.createElement(
+	                            "figure",
+	                            { className: "profile" },
+	                            _react2.default.createElement("img", { className: "profile-avatar", alt: "", src: this.props.event.cover_picture })
+	                        ),
+	                        _react2.default.createElement(
+	                            "h1",
+	                            { className: "card-title mt-3" },
+	                            this.props.event.name
+	                        ),
+	                        _react2.default.createElement(
+	                            "p",
+	                            null,
+	                            this.props.event.category
+	                        ),
+	                        _react2.default.createElement(
+	                            "div",
+	                            { className: "card-text" },
+	                            _react2.default.createElement(
+	                                "p",
+	                                null,
+	                                this.props.event.description
+	                            )
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "div",
+	                        { className: "card-footer" },
+	                        _react2.default.createElement(
+	                            "small",
+	                            null,
+	                            this.props.event.start_time,
+	                            " | ",
+	                            this.props.event.end_time
+	                        ),
+	                        _react2.default.createElement("br", null),
+	                        _react2.default.createElement(
+	                            "small",
+	                            null,
+	                            "*Go to calendar to see events*"
+	                        )
+	                    )
+	                )
+	            );
+	        }
+	    }]);
+
+	    return EventCard;
 	}(_react2.default.Component);
 
 	exports.default = EventCard;
