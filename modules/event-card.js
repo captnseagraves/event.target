d4d4a7358a1e8c13790720d766e773c410df7876
TomGarrison <Tomgarrison2011@gmail.com>
2017-05-25 09:51:25 -0600

    update cards

--- modules/event-card.js
+++ modules/event-card.js
@@ -7,7 +7,7 @@ console.log(this.props.event);
     return (
 <div className="col-sm-6 col-md-4 col-lg-3 mt-4">
     <div className="card">
-        <img className="card-img-top" src={this.props.event.cover_picture}></img><br></br>
+        <img className="card-img-top" src={this.props.event.event_cover_picture}></img><br></br>
         <div className="card-block">
         <p>click the calendar to add event</p>
             <figure className="profile">
