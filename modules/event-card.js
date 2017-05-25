1948dcbd29563599f34ddec29668d5e5251fc72d
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-05-25 09:46:56 -0600

    resolve pull

--- modules/event-card.js
+++ modules/event-card.js
@@ -3,27 +3,15 @@ import React from 'react'
 class EventCard extends React.Component {
 
   render() {
-console.log(this.props.event);
+
+    console.log('IN EVENTCARD')
     return (
-<div className="col-sm-6 col-md-4 col-lg-3 mt-4">
-    <div className="card">
-        <img className="card-img-top" src={this.props.event.cover_picture}></img><br></br>
-        <div className="card-block">
-            <figure className="profile">
-                <img className="profile-avatar" alt="" src={this.props.event.cover_picture}></img>
-            </figure>
-            <h1 className="card-title mt-3">{this.props.event.name}</h1>
-            <p>{this.props.event.category}</p>
-            <div className="card-text">
-              <p>{this.props.event.description}</p>
-            </div>
-        </div>
-        <div className="card-footer">
-            <small>{this.props.event.start_time} | {this.props.event.end_time}</small><br></br>
-            <small>*Go to calendar to see events*</small>
-        </div>
-    </div>
-</div>
+
+          <div>
+            <h1>{this.props.event.event_name}</h1>
+            <hr></hr>
+          </div>
+
        )
   }
 
